161111ea9055659d17042c5511d869984d590986
PriyankaKaria <48031076+PriyankaKaria@users.noreply.github.com>
2020-09-28 15:56:30 +0200

    Add files via upload
    Asset

---
+++ Assets/Scripts/GridSpace.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Globalization;
+
+public class GridSpace : MonoBehaviour
+{
+    public Button button;
+    //public Text buttonText;
+    public string text;
+    // public string playerSide;
+    public Image image;
+
+    private GameController gameController;
+
+    
+    public void Setspace()
+    {
+        if(gameController.playerMove==true)
+        {
+            text = gameController.GetPlayerSide();
+            image.sprite = gameController.GetPlayerSideImage();
+            button.interactable = false;
+            gameController.EndTurn();
+        }
+         
+    }
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+    public void ResetGridSpace()
+    {
+        text = "";
+        image.sprite = null;
+    }
+}
